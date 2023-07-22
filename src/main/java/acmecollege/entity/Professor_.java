8f8ae46f55cd62db98594ab984ace84bc22419f9
AbdulMajeed <chowdhury4554@gmail.com>
2023-07-22 17:55:41 -0400

    project copy

---
+++ src/main/java/acmecollege/entity/Professor_.java
@@ -0,0 +1,15 @@
+package acmecollege.entity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SetAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="Dali", date="2022-08-28T12:02:14.629-0400")
+@StaticMetamodel(Professor.class)
+public class Professor_ {
+	public static volatile SingularAttribute<Professor, String> firstName;
+	public static volatile SingularAttribute<Professor, String> lastName;
+	public static volatile SingularAttribute<Professor, String> department;
+	public static volatile SetAttribute<Professor, CourseRegistration> courseRegistrations;
+}
